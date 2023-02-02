b4cbd3065e27a9423576994564a2b93415d8bacb
Nadeemraffe <123829445+Nadeemraffe@users.noreply.github.com>
2023-02-02 05:24:26 -0800

    Add files via upload

---
+++ ASM2/app.js
@@ -0,0 +1,21 @@
+// Question No1'2'3'4
+var Name ="jhone Done"
+var year ="15 year old"
+var mobile ="Certified Mobile Application development"
+alert(Name)
+alert(year)
+alert(mobile)
+// Question No5
+var pizza="Pizza\nPizz\nPiz\nPi\nP "
+alert(pizza)
+// Question No6
+var email="My email address is example@gmail.com"
+alert(email)
+// Question No7
+var Message="I am Trying to learn From thr book\nA Smarter way to learn Javascript"
+alert(Message)
+// Question No8
+document.write("Yah! can write HTML content throught javaScript")
+// Question No9
+var line="-----------------@NADEEM@---------------"
+alert(line)
